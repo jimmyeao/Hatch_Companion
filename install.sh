a16c8408b4deaa70f573f163b8738e0ad2ae8276
Jimmy White <jimmyeao@users.noreply.github.com>
2023-02-02 14:30:39 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,15 @@ cd bcm2835-1.71/
 sudo ./configure && sudo make && sudo make check && sudo make install
 # For more information, please refer to the official website: http://www.airspayce.com/mikem/bcm2835/
 cd ~/
+sudo apt install git -y
 git clone https://github.com/WiringPi/WiringPi
 cd WiringPi
 ./build
+cd ~/
 sudo apt-get install python3-pip -y
 sudo apt-get install python3-pil -y
 sudo apt-get install python3-numpy -y
 sudo pip3 install RPi.GPIO -y
 sudo pip3 install spidev -y
+wget https://github.com/jimmyeao/Hatch_Companion/blob/main/Hatch.py
+python3 Hatch.py
