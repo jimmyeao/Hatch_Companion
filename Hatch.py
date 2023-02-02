3bf1eb8f9960651bab4627c0e08b7d7414344adf
Jimmy White <jimmyeao@users.noreply.github.com>
2023-02-02 17:00:33 +0000

    ratelimiting

--- Hatch.py
+++ Hatch.py
@@ -68,7 +68,18 @@ def create_awayimage(text1, color = (0,0,0)):
     text_width, text_height = draw.textsize(text1, font=Font3)
     draw.text(((disp.width-text_width)/2, (disp.height-text_height)/2), text1, fill = color,font = Font3)
     return awayimage.rotate(0)      
-
+    
+def create_offline(text1, color = (68,71,145)):
+    offline = Image.new("RGB", (disp.width, disp.height), "BLACK")
+    draw = ImageDraw.Draw(offline)
+    draw.arc((1,1,239,239),0, 360, fill = (68,71,145),width = 20)
+    draw.arc((2,2,238,238),0, 360, fill = (68,71,145),width = 20)
+    draw.arc((3,3,237,237),0, 360, fill = (68,71,145),width = 20)
+    Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
+    text_width, text_height = draw.textsize(text1, font=Font3)
+    draw.text(((disp.width-text_width)/2, (disp.height-text_height)/2), text1, fill = color,font = Font3)
+    return offline.rotate(0)     
+    time.sleep(1.5)
 
 
 @app.route('/')
@@ -92,7 +103,10 @@ def showimage():
         image = create_availableimage(text1)
     elif image_type == 'away':
         text1 = request.form.get('text1')
-        image = create_awayimage(text1)    
+        image = create_awayimage(text1)  
+    elif image_type == 'offline':
+        text1 = request.form.get('text1')
+        image = create_offline(text1)         
     elif image_type == 'test':
         return 'ok'
             
