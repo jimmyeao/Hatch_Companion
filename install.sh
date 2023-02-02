d70fa1bf1a81dc5befce2aaac13e22a1f2bea4ac
Jimmy White <jimmyeao@users.noreply.github.com>
2023-02-02 17:50:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ tar zxvf bcm2835-1.71.tar.gz
 cd bcm2835-1.71/
 sudo ./configure && sudo make && sudo make check && sudo make install
 # For more information, please refer to the official website: http://www.airspayce.com/mikem/bcm2835/
-cd ~/
+cd ..
 sudo apt install git -y
 git clone https://github.com/WiringPi/WiringPi
 cd WiringPi
 ./build
-cd ~/
+cd ..
 sudo apt-get install python3-pip -y
 sudo apt-get install python3-pil -y
 sudo apt-get install python3-numpy -y
