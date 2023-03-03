f6320e6e03ec05a18619e4720e6b845013cd5f96
jimmyeao <jimmyeao@gmail.com>
2023-03-03 10:52:11 +0000

    fix for corrupt graphics when multiple api requests are made in quick succession

--- Hatch.py
+++ Hatch.py
@@ -5,12 +5,12 @@ import logging
 import spidev as SPI
 import flask
 from flask import Flask, request
-
+import threading
 from lib import LCD_1inch28
 from PIL import Image,ImageDraw,ImageFont
 
-app = Flask(__name__)
-
+# Define a lock object
+display_lock = threading.Lock()
 
 # Raspberry Pi pin configuration:
 RST = 27
@@ -26,70 +26,100 @@ disp = LCD_1inch28.LCD_1inch28()
 disp.Init()
 # Clear display.
 disp.clear()
+# Constants
+
+FONT_02 = ImageFont.truetype("Font/Font01.ttf", 32)
+FONT_03 = ImageFont.truetype("Font/Font02.ttf", 35)
+COLOR_RED = (188, 47, 73)
+COLOR_GREEN = (118, 165, 38)
+COLOR_YELLOW = (252, 209, 22)
+COLOR_BLUE = (68, 71, 145)
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+
+app = Flask(__name__)
    
 ################# Definitions for screens ########################        
 def create_startup_animation(disp):
     animation_frames = []
-    font = ImageFont.truetype("Font/Font02.ttf", 35)
+    font = FONT_02
     for i in range(0, 360, 5):
-        frame = Image.new("RGB", (disp.width, disp.height), "BLACK")
+        frame = Image.new("RGB", (disp.width, disp.height), COLOR_BLACK)
         draw = ImageDraw.Draw(frame)
-        draw.arc((25, 25, 215, 215), 0, 360, fill=(255, 255, 255), width=5)
-        draw.arc((25, 25, 215, 215), 0, i, fill=(0, 255, 0), width=5)
-        draw.text((disp.width // 2 - 75, disp.height // 2 - 20), "Loading...", fill=(255, 255, 255), font=font)
+        draw.arc((25, 25, 215, 215), 0, 360, fill=COLOR_WHITE, width=5)
+        draw.arc((25, 25, 215, 215), 0, i, fill=COLOR_GREEN, width=5)
+        draw.text(
+            (disp.width // 2 - 75, disp.height // 2 - 20),
+            "Loading...",
+            fill=COLOR_WHITE,
+            font=FONT_03,
+        )
         animation_frames.append(frame)
     return animation_frames
-def create_dndimage(text3, text4, color = (255,255,255)):
+def create_dndimage(text3, text4, color=COLOR_WHITE):
     dndimage = Image.new("RGB", (disp.width, disp.height), "BLACK")
     draw = ImageDraw.Draw(dndimage)
-    draw.arc((1,1,239,239),0, 360, fill = (188,47,73),width = 20)
-    draw.arc((2,2,238,238),0, 360, fill = (188,47,73),width = 20)
-    draw.arc((3,3,237,237),0, 360, fill = (188,47,73),width = 20)
-    draw.line([(40,120),(200,120)],fill = (188,47,73),width = 20)
-    Font1 = ImageFont.truetype("Font/Font01.ttf",25)
-    Font2 = ImageFont.truetype("Font/Font01.ttf",35)
-    Font3 = ImageFont.truetype("Font/Font02.ttf",32)
-    draw.text((81, 70), text3, fill = color,font = Font3)
-    draw.text((80, 130), text4, fill = color,font = Font3)
+    draw.arc((1, 1, 239, 239), 0, 360, fill=COLOR_RED, width=20)
+    draw.arc((2, 2, 238, 238), 0, 360, fill=COLOR_RED, width=20)
+    draw.arc((3, 3, 237, 237), 0, 360, fill=COLOR_RED, width=20)
+    draw.line([(40, 120), (200, 120)], fill=COLOR_RED, width=20)
+    draw.text((81, 70), text3, fill=color, font=FONT_03)
+    draw.text((80, 130), text4, fill=color, font=FONT_03)
     return dndimage.rotate(0)
     
-def create_busyimage(text1, color = (255,255,255)):
-    busyimage = Image.new("RGB", (disp.width, disp.height), "BLACK")
+def create_busyimage(text1, color=COLOR_WHITE):
+    busyimage = Image.new("RGB", (disp.width, disp.height), COLOR_BLACK)
     draw = ImageDraw.Draw(busyimage)
-    draw.ellipse((10, 10, disp.width-10, disp.height-10), fill=(188,47,73))
-    Font3 = ImageFont.truetype("Font/Font02.ttf",32)
-    text_width, text_height = draw.textsize(text1, font=Font3)
-    draw.text(((disp.width-text_width)/2, (disp.height-text_height)/2), text1, fill = color,font = Font3)
+    draw.ellipse((10, 10, disp.width-10, disp.height-10), fill=COLOR_RED)
+    text_width, text_height = draw.textsize(text1, font=FONT_03)
+    draw.text(
+        ((disp.width-text_width)//2, (disp.height-text_height)//2),
+        text1,
+        fill=color,
+        font=FONT_03,
+    )
     return busyimage.rotate(0)
-    
-def create_availableimage(text1, color = (0,0,0)):
-    availableimage = Image.new("RGB", (disp.width, disp.height), "BLACK")
+
+def create_availableimage(text1, color=COLOR_BLACK):
+    availableimage = Image.new("RGB", (disp.width, disp.height), COLOR_BLACK)
     draw = ImageDraw.Draw(availableimage)
-    draw.ellipse((10, 10, disp.width-10, disp.height-10), fill=(118, 165, 38))
-    Font3 = ImageFont.truetype("Font/Font02.ttf",32)
-    text_width, text_height = draw.textsize(text1, font=Font3)
-    draw.text(((disp.width-text_width)/2, (disp.height-text_height)/2), text1, fill = color,font = Font3)
-    return availableimage.rotate(0)    
-    
-def create_awayimage(text1, color = (0,0,0)):
-    awayimage = Image.new("RGB", (disp.width, disp.height), "BLACK")
+    draw.ellipse((10, 10, disp.width-10, disp.height-10), fill=COLOR_GREEN)
+    text_width, text_height = draw.textsize(text1, font=FONT_03)
+    draw.text(
+        ((disp.width-text_width)//2, (disp.height-text_height)//2),
+        text1,
+        fill=color,
+        font=FONT_03,
+    )
+    return availableimage.rotate(0)
+
+def create_awayimage(text1, color=COLOR_BLACK):
+    awayimage = Image.new("RGB", (disp.width, disp.height), COLOR_BLACK)
     draw = ImageDraw.Draw(awayimage)
-    draw.ellipse((10, 10, disp.width-10, disp.height-10), fill=(252, 209, 22))
-    Font3 = ImageFont.truetype("Font/Font02.ttf",32)
-    text_width, text_height = draw.textsize(text1, font=Font3)
-    draw.text(((disp.width-text_width)/2, (disp.height-text_height)/2), text1, fill = color,font = Font3)
-    return awayimage.rotate(0)      
-    
-def create_offline(text1, color = (68,71,145)):
-    offline = Image.new("RGB", (disp.width, disp.height), "BLACK")
+    draw.ellipse((10, 10, disp.width-10, disp.height-10), fill=COLOR_YELLOW)
+    text_width, text_height = draw.textsize(text1, font=FONT_03)
+    draw.text(
+        ((disp.width-text_width)//2, (disp.height-text_height)//2),
+        text1,
+        fill=color,
+        font=FONT_03,
+    )
+    return awayimage.rotate(0)
+
+def create_offline(text1, color=COLOR_BLUE):
+    offline = Image.new("RGB", (disp.width, disp.height), COLOR_BLACK)
     draw = ImageDraw.Draw(offline)
-    draw.arc((1,1,239,239),0, 360, fill = (68,71,145),width = 20)
-    draw.arc((2,2,238,238),0, 360, fill = (68,71,145),width = 20)
-    draw.arc((3,3,237,237),0, 360, fill = (68,71,145),width = 20)
-    Font3 = ImageFont.truetype("Font/Font02.ttf",32)
-    text_width, text_height = draw.textsize(text1, font=Font3)
-    draw.text(((disp.width-text_width)/2, (disp.height-text_height)/2), text1, fill = color,font = Font3)
-    return offline.rotate(0)     
+    draw.arc((1, 1, 239, 239), 0, 360, fill=COLOR_BLUE, width=20)
+    draw.arc((2, 2, 238, 238), 0, 360, fill=COLOR_BLUE, width=20)
+    draw.arc((3, 3, 237, 237), 0, 360, fill=COLOR_BLUE, width=20)
+    text_width, text_height = draw.textsize(text1, font=FONT_03)
+    draw.text(
+        ((disp.width-text_width)//2, (disp.height-text_height)//2),
+        text1,
+        fill=color,
+        font=FONT_03,
+    )
+    return offline.rotate(0)
     
     
 def waitingimage(text1, color = (68,71,145)):
@@ -98,21 +128,27 @@ def waitingimage(text1, color = (68,71,145)):
     draw.arc((1,1,239,239),0, 360, fill = (68,71,145),width = 20)
     draw.arc((2,2,238,238),0, 360, fill = (68,71,145),width = 20)
     draw.arc((3,3,237,237),0, 360, fill = (68,71,145),width = 20)
-    Font3 = ImageFont.truetype("Font/Font02.ttf",32)
-    text_width, text_height = draw.textsize(text1, font=Font3)
-    draw.text(((disp.width-text_width)/2, (disp.height-text_height)/2), text1, fill = color,font = Font3)
+    text_width, text_height = draw.textsize(text1, font=FONT_03)
+    draw.text(((disp.width-text_width)/2, (disp.height-text_height)/2), text1, fill = color,font = FONT_03)
     return waiting.rotate(0)     
 
+def create_errorimage(text1, text2, color=COLOR_RED):
+    errorimage = Image.new("RGB", (disp.width, disp.height), COLOR_BLACK)
+    draw = ImageDraw.Draw(errorimage)
+    draw.text((20, 50), text1, fill=color, font=FONT_03)
+    draw.text((20, 100), text2, fill=color, font=FONT_03)
+    return errorimage.rotate(0)
+    
 def create_onthephone_image(text1, color=(255, 255, 255)):
     onthephone = Image.new("RGB", (disp.width, disp.height), "BLACK")
     draw = ImageDraw.Draw(onthephone)
     draw.ellipse((10, 10, disp.width-10, disp.height-10), fill=(188,47,73))
-    Font3 = ImageFont.truetype("Font/Font02.ttf", 32)
-    draw.text((30, 100), "On             the", fill=color, font=Font3)
-    draw.text((90, 145), "Phone", fill=color, font=Font3)
-    phone_image = Image.open("phone.png").resize((80, 80), Image.ANTIALIAS).rotate(90)
+    draw.text((30, 100), "On             the", fill=color, font=FONT_03)
+    draw.text((90, 145), "Phone", fill=color, font=FONT_02)
+    phone_image = Image.open("phone.png").resize((80, 80), resample=Image.ANTIALIAS).rotate(90)
     onthephone.paste(phone_image, (80, 60), phone_image)
-    return onthephone.rotate(0) 
+    return onthephone.rotate(0)
+
         
 def show_startup_animation(disp):
     animation_frames = create_startup_animation(disp)
@@ -156,9 +192,15 @@ def showimage():
         return 'Not a POST request'
 
     print("Matched image_type:", image_type)       
-    disp.ShowImage(image)    
+     # Acquire the lock before drawing to the display
+    display_lock.acquire()
+    try:
+        disp.ShowImage(image)
+    finally:
+        # Release the lock when done drawing
+        display_lock.release()
+    
     return 'Showing image: {}'.format(image_type)
-    time.sleep(1.5)
 ############ main ################
 if __name__ == '__main__':
     show_startup_animation(disp)
